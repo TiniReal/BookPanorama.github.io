92ceff078fb053586ce7d05528c03d5f9adfa674
Thomas Tinelli <97833491+TiniReal@users.noreply.github.com>
2024-02-28 19:27:01 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development', // Imposta il mode su 'development'
+    mode: 'development',
     entry: './assets/js/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'source-map', // Aggiunta del devtool source-map
 };
+
+
 
 
 
